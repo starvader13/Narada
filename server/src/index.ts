4bb0b85254bcad6d69dae4c0a5d0c513b8b94367
Starvader <ashutoshgupta1311@gmail.com>
2024-09-28 17:59:05 +0530

    Add user-routes and api/v1 routes
    Signed-off-by: Starvader <ashutoshgupta1311@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
 import StatusCodes from "./enums/StatusCodes";
+import mainRouter from "./router/mainRouter";
 
 const app = express();
+
+app.use("/api/v1", mainRouter);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
---
+++ server/src/router/userRouter.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.post("/signin", (req: Request, res: Response) => {});
+
+router.post("/signup", (req: Request, res: Response) => {});
+
+export default router;
