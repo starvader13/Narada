fb4615f1dafdcf61c610c2e0aa310c7baa231c13
Starvader <ashutoshgupta1311@gmail.com>
2024-10-01 06:38:29 +0530

    fix logic for confirming password
    Signed-off-by: Starvader <ashutoshgupta1311@gmail.com>

--- server/src/middleware/signup/doesUserExist.ts
+++ server/src/middleware/signup/doesUserExist.ts
@@ -8,7 +8,7 @@ const doesUserExist = async (
   res: Response,
   next: NextFunction,
 ) => {
-  const email: string = req.body.email;
+  const { email, password } = req.body;
   const user: User | null = await findUser(email);
 
   if (user == null) {
@@ -16,6 +16,13 @@ const doesUserExist = async (
       msg: "The email is not registered. Please Sign-Up",
     });
     return;
+  }
+
+  if (user.password !== password) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      msg: "Either the email or the password is incorrect",
+      error: "The password is incorrect",
+    });
   }
 
   return next();
